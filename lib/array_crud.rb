f7752e98644221c663d063de66b61fd79c0f3876
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-12 20:53:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["ja", "Masha", "ljubav", "okice"]
+  #array = ["I", "am", "really", "learning"]
   array << "arrays!"
 end
 
