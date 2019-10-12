477899e46ae310d49d7e2f8732c098816389dfe8
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-12 20:52:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["ja", "am", "ljubav", "okice"]
+  array = ["I", "am", "ljubav", "okice"]
 end
 
 def add_element_to_end_of_array(array, element)
