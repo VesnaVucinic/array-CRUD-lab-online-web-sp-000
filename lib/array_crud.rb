97dd47d76b921e09672e8adf4a47c6aee18d8a2d
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-12 20:35:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,9 @@ def create_an_array
   my_array = [ja, Masha, ljubav, okice]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(my_array, srce)
+  my_array = [ja, Masha, ljubav, okice]
+  my_array << srce
 end
 
 def add_element_to_start_of_array(array, element)
